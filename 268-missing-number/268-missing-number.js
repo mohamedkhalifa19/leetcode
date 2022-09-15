061bddecec8c414aef72afaad87fde8d8d87e7c3
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2022-09-16 01:46:24 +0200

    Time: 184 ms (16.11%), Space: 49.3 MB (5.26%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -3,18 +3,24 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
+    
+// sort array elements
+    
 nums = nums.sort((x,y)=>x-y);
 
 const results =new Map();
 
-// create map to contain 
+// create map to contain array elements 
 for(let i=0;i<nums.length;i++){
   results.set(i,nums[i])
 }
+    // counter to check if all numbers in range [0,n] is found or not
+  
     let count =0;
+    
     for(let i=0;i<=nums.length;i++){
-      if(results.get(i)!=count)
-      return count;
+      if(results.get(i)!=count) // if number is not found 
+      return count; // return missing number
       count++;
       }
    
