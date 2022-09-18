7484187a01efc2b6755e1cd8e0a5398fdde5d422
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2022-09-18 07:12:21 +0200

    Time: 118 ms (21.93%), Space: 42.1 MB (82.90%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -2,18 +2,9 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
-};/**
- * @param {string} s
- * @return {boolean}
- */
  var isValid = function(s) {
     let ans=[];
     for (let i = 0; i < s.length; i++) {
-    if(s[i]=='(' || s[i]=='{' || s[i]=='[')
-     ans.push(s[i]);
-    else{
       if (s[i]==')' && ans[ans.length-1]=='(')
        ans.pop();
       else if (s[i]==']' && ans[ans.length-1]=='[')
@@ -22,7 +13,6 @@ var isValid = function(s) {
        ans.pop();
        else
        ans.push(s[i]);
-    }
 }
 return (ans.length==0);
 };
