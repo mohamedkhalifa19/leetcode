f1853af6c154093991bb3f42f422a2e8f5f8239d
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2022-09-21 07:34:19 +0200

    Time: 163 ms (56.26%), Space: 55.9 MB (24.33%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,33 +3,16 @@
  * @return {number[]}
  */
  var productExceptSelf = function(nums) {
-    let answer = nums;
-    let multiplication1 = 1;
-    let multiplication2 =1;
-    let zeroes= 0;
-    for (let i = 0; i < nums.length; i++)
-    {
-    if (nums[i]!=0)
-    multiplication2*=nums[i];
-    else
-    zeroes++;
-    multiplication1*=nums[i];
-    if(zeroes>1)
-   return  answer.fill(0,0,nums.length);
-    }
-    multiplication1 = Math.min(Math.max(-(2**31-1),multiplication1),2**31);
-    multiplication2 = Math.min(Math.max(-(2**31-1),multiplication2),2**31);
-    for (let i = 0; i < nums.length; i++)
-    {
-    if (nums[i] !=0){
-    if(multiplication1==0)
-    answer[i]=0;
-    else
-    answer[i] = multiplication1/nums[i];
-    }
-   
-    else
-    answer[i] = multiplication2;
-    }
-    return answer;
+  let ans=[];
+  let prefix=1;
+  for (let i=0; i<nums.length; i++){
+    ans[i]= prefix;
+    prefix*=nums[i];
+  }
+  let postfix =1;
+  for (let i=nums.length-1; i>=0; i--){
+    ans[i]*= postfix;
+    postfix*=nums[i];
+  }
+  return ans;
 };
