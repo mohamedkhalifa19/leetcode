c07a03015b07d1114b8904a8fa1e4955860bee89
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2022-09-23 17:19:49 +0200

    Time: 94 ms (36.78%), Space: 41.9 MB (56.45%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.js
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.js
@@ -14,13 +14,12 @@
 var guessNumber = function(n) {
     let left = 1 , right = n;
     while (left<=right){
-        let mid = left + Math.floor((right-left)/2)
+        let mid =Math.floor((left+right)/2)
         if(guess(mid)==0)
             return mid
         else if(guess(mid)==-1)
             right = mid -1;
         else 
-            left =mid+1;
-            
+            left =mid+1;   
     }
 };
