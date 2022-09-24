01579a477bbae93145b8dce3c84edddb93ef7399
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2022-09-24 13:14:07 +0200

    Time: 121 ms (45.42%), Space: 51 MB (92.56%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
@@ -4,15 +4,17 @@
  */
 var peakIndexInMountainArray = function(arr) {
     
-    let maxx_num = Math.max(...arr)
+    let ans , left =  0 ,right = arr.length-1;
     
-//     let left = 0 , right = arr.length-1;
+    while (left<=right){
+        let mid = Math.floor((left+right)/2);
+        if (arr[mid]<=arr[mid+1]){
+            ans = mid+1;
+            left = mid+1;
+        }
+        else
+            right = mid-1
+    }
     
-//     while(left<=right){
-//         let mid = Math.floor((left+right/2))
-        
-//         if (arr[mid])
-//     }
-    
-    return arr.indexOf(maxx_num)
+    return ans
 };
