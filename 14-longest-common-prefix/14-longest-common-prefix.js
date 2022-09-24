81e42b32f4755ea42337e2f0af73036af6a19195
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2022-09-24 06:45:07 +0200

    Time: 92 ms (62.65%), Space: 42.3 MB (70.57%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -3,20 +3,16 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
+    strs.sort();
+    let size = strs.length
+    let end = strs[0].length;
     
-    strs.sort()
-    
-    let ans = strs[0].split('');
-    
-    for(let s of strs){
-        for(let i=0;i<s.length;i++){
-            if(s[i]!=ans[i]){
-                ans.fill('',i);
-            }
-        }
-    }
-    
-    
-    
-    return ans.join('');
+    if(size==0) return ""
+    if(size==1) return strs[0]
+    let ans=""
+    let i =0
+    while(i<end&&strs[0][i]==strs[size-1][i])
+        i++;
+     ans = strs[0].substring(0, i);
+    return ans;
 };
