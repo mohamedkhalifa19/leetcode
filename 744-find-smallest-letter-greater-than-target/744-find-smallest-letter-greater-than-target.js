3e5adcdf46007aeb7ac0bb3413bcec1d0d07306f
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2022-09-24 17:24:33 +0200

    Time: 98 ms (53.73%), Space: 44.1 MB (32.96%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
@@ -4,8 +4,6 @@
  * @return {character}
  */
 var nextGreatestLetter = function(letters, target) {
-    if (target =='z' && letters.includes('a'))
-        return 'a'
     let left = 0 , right = letters.length-1 ,ans = letters[0];
        while (left<=right){
            let mid = left + Math.floor((right-left)/2);
