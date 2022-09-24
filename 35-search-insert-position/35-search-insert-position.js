15dd3a44a3a16aecaafeac37035df950cf339f9d
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2022-09-24 11:43:33 +0200

    Time: 53 ms (99.22%), Space: 41.8 MB (89.16%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,16 +4,18 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let left = 0 , right = nums.length-1;
-    if (target < nums[left]) return 0;
-    if (target > nums[right]) return nums.length;
-    while(left<=right){
-        let mid = (left+right)>>1;
-        if(target == nums[mid]) return mid;
-        else if(target>nums[mid])
+    let ans = 0 , left = 0 , right = nums.length-1;
+    
+    while (left<=right){
+        let mid = left + Math.floor((right-left)/2)
+        if (nums[mid]==target) 
+            return mid
+        else if(nums[mid]<target)
             left = mid+1;
-        else 
-            right = mid-1;
+        else
+            right =mid-1
+        
+            
     }
-    return left;
+    return left
 };
