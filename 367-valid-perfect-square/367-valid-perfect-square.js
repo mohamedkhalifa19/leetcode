4ef0188ecb25a4192d1f8408e2a57e737c716f84
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2022-09-25 17:23:37 +0200

    Time: 102 ms (33.99%), Space: 42 MB (53.75%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.js
+++ 367-valid-perfect-square/367-valid-perfect-square.js
@@ -3,8 +3,8 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    let left = 0 , right = num;
-    
+    let left = 0 , right = Math.floor(num/2);
+    if(num==0||num==1) return true;
     while(left<=right){
         let m= left + Math.floor((right-left)/2)
         if(m*m == num) return true;
