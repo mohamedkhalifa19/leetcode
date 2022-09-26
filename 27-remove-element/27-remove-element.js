a2140e1e5be14e89ec1f187141c9bd7f66e90c91
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2022-09-26 20:39:25 +0200

    Time: 84 ms (66.69%), Space: 41.9 MB (68.29%) - LeetHub

--- 27-remove-element/27-remove-element.js
+++ 27-remove-element/27-remove-element.js
@@ -4,9 +4,11 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    for (let i =nums.length-1;i>=0;i--){
-        if(nums[i] === val)
-          nums.splice(i,1);
-    }
-   return nums.length;
+   let slow=0 ,fast=0;
+for(let i=0;i<nums.length;i++){
+    nums[slow] = nums[fast];
+    if(nums[slow]!=val) slow++;
+    fast++;
+}
+    return slow;
 };
