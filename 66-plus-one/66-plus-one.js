2ef827f1a17cb8ff69a601d65f0f4ef8e47877c0
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2022-09-29 14:57:57 +0200

    Time: 94 ms (47.97%), Space: 41.7 MB (91.22%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -6,11 +6,12 @@ var plusOne = function(digits) {
   let carry = 0 ,i =digits.length-1;
    while(i>=0){
        let sum = ((digits[i]-'0')+1);
-       digits[i] =sum%10;
+       digits[i]=sum%10;
        carry =Math.floor(sum/10);
        if(carry===0) break;
        i--;
    }
-    if(carry===1) digits.unshift(carry) 
+    if(carry===1)
+        digits.unshift(carry) 
     return digits;
 };
